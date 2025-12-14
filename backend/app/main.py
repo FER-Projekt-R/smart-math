1398ebf5ed9fb12ed179cdb9f446d192efbb4819
Fran Blažević <fran.blazevic0@gmail.com>
2025-12-15 00:28:56 +0100

    Napravljen adaptivan model + podesene rute u backendu za predikciju i feedback, dodan servis state modela

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import health, test_db, ml_predict
+from .routers import health, test_db, ml_predict, ml_feedback
 
 app = FastAPI(title="SmartMath API", version="0.1.0")
 
@@ -7,6 +7,7 @@ app = FastAPI(title="SmartMath API", version="0.1.0")
 app.include_router(health.router, prefix="/health", tags=["health"])
 app.include_router(test_db.router, prefix="/test", tags=["test"])
 app.include_router(ml_predict.router, prefix="/difficulty", tags=["ML Model - predict difficulty"])
+app.include_router(ml_feedback.router, prefix="/difficulty", tags=["ML Model - get feedback and update model"])
 
 @app.get("/")
 def root():
