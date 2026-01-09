f795b7e9d685ebd17f9984739a47f2212cfb2d93
mia-sagovac <mia.sagovac@fer.hr>
2026-01-09 20:37:53 +0100

    frontend url

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,6 +29,7 @@ fastapi_app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        FRONTEND_URL,
     ],
     allow_credentials=True,
     allow_methods=["*"],
